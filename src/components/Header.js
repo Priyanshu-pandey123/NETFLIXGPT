41e7942f37d431923db13ea99e88dce2a6bb9ea2
priyanshu pandey <prince678p@gmail.com>
2024-08-02 23:51:35 +0530

    set up form

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,16 @@ import { LOGO } from '../utils/constant'
 
 const Header = () => {
   return (
-    <div className='absolute bg-gradient-to-b from-transparent to-black h-18 w-full'>
-      <img src={LOGO}  className='h-20 w-25'
+    <div className='absolute bg-gradient-to-b from-transparent to-black h-18 w-full z-10 flex justify-between  '>
+
+     <div>
+     <img src={LOGO}  className='h-20 w-25'
       alt="not found"/>
+     </div>
+
+    <div className=' my-4 text-white rounded-lg'>
+    <button className='bg-purple-700 p-3 rounded-lg mx-5 '>Sign Out</button>
+    </div>
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { IMG_URL } from '../utils/constant'
 
 const Login = () => {
+    const [sign,setSign]=useState(false);
+    const name=useRef(null);
+    const email=useRef(null);
+    const  PassWord=useRef(null);
+
+    const handleForm=()=>{
+        console.log(email.current.value,PassWord.current.value,name.current.value);
+        vaidateForm=formValidator(email.current.value,PassWord.current.value);
+    }
+
   return (
     <div>
      <Header/>
-     <div>
+     <div className='absolute'>
         <img src={IMG_URL}
         className='h-full w-full '
          alt="not found"/>
      </div>
+     
+        
+         <form className='bg-black  bg-opacity-80 absolute w-3/12  mt-40  rounded-lg flex flex-col mx-auto left-0 right-0  text-white px-4 py-4' onSubmit={(e)=>e.preventDefault()}>
+         <h1 className='text-4xl p-2 m-3'>SIGN IN</h1>
+         { sign &&  <input type='text' ref={name} placeholder='Enter Name...' className='p-4 my-2 mx-2 rounded-lg bg-gray-600'  required/> }
+         <input type='text' ref={email} placeholder='Enter Email...' className='p-4 my-2 mx-2 rounded-lg bg-gray-600' required />
+            <input type='text' ref={PassWord} placeholder='Enter PassWord...' className='p-4 my-2 mx-2 bg-gray-500 rounded-lg' required />
+            <button className='p-4 my-2 mx-2 bg-red-700 rounded-lg' type='text' onClick={handleForm} >SIGN</button>
+            <p className='p-2 m-2 text-red-600 cursor-pointer' onClick={()=>setSign(!sign)}>
+                {sign ? "signUp/register" : "Sign in"} 
+                  
+                  </p>
+        </form>
+ 
     </div>
   )
 }
